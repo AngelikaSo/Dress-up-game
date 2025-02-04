ead3997544798b3061b4ddf7f386bca2dca8882e
Angelika Sowka <sowkaangelika@gmail.com>
2025-02-03 22:16:44 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import Desktop from "./Components/Desktop";
 function App() {
   return (
     <div className="App">
       <h1>Test</h1>
+      <Desktop />
     </div>
   );
 }
---
+++ src/Components/Desktop.js
@@ -0,0 +1,13 @@
+const Desktop = () => {
+  return (
+    <div>
+      <nav>
+        <p>gadu-gadu</p>
+        <p>games</p>
+        <p>my computer</p>
+        <p>recycle bin</p>
+      </nav>
+    </div>
+  );
+};
+export default Desktop;
